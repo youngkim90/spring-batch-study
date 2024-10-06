88543ba305bce67964685eab65806376b0dc8695
young-kim <ceo.youngkim@gmail.com>
2024-10-06 21:11:25 +0900

    first commit

---
+++ src/main/java/com/schooldevops/springbatch/batchstudy/BatchStudyApplication.java
@@ -0,0 +1,13 @@
+package com.schooldevops.springbatch.batchstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schooldevops/springbatch/batchstudy/BatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooldevops.springbatch.batchstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
