ca6c0712dafe2bf9b8c5bfe4e43e092eb2a07f3e
young-kim <ceo.youngkim@gmail.com>
2024-11-19 19:28:58 +0900

    feat: 7주차 - MyBatisPagingItemReader/MyBatisBatchItemWriter 구현

---
+++ src/main/java/com/schooldevops/springbatch/batchstudy/jobs/mybatis/MyBatisReaderJobConfig.java
@@ -0,0 +1,81 @@
+package com.schooldevops.springbatch.batchstudy.jobs.mybatis;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.batch.MyBatisPagingItemReader;
+import org.mybatis.spring.batch.builder.MyBatisPagingItemReaderBuilder;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.file.FlatFileItemWriter;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import com.schooldevops.springbatch.batchstudy.jobs.jpa.CustomerItemProcessor;
+import com.schooldevops.springbatch.batchstudy.models.Customer;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+// @Configuration
+public class MyBatisReaderJobConfig {
+
+	/**
+	 * CHUNK 크기를 지정한다.
+	 */
+	public static final int CHUNK_SIZE = 2;
+	public static final String ENCODING = "UTF-8";
+	public static final String MYBATIS_CHUNK_JOB = "MYBATIS_CHUNK_JOB";
+
+	// @Autowired
+	SqlSessionFactory sqlSessionFactory;
+
+	@Bean
+	public MyBatisPagingItemReader<Customer> myBatisItemReader() throws Exception {
+
+		return new MyBatisPagingItemReaderBuilder<Customer>()
+			.sqlSessionFactory(sqlSessionFactory)
+			.pageSize(CHUNK_SIZE)
+			.queryId("com.schooldevops.springbatch.batchstudy.jobs.selectCustomers")
+			.build();
+	}
+
+	@Bean
+	public FlatFileItemWriter<Customer> CustomerCursorFlatFileItemWriter() {
+		return new FlatFileItemWriterBuilder<Customer>()
+			.name("CustomerCursorFlatFileItemWriter")
+			.resource(new FileSystemResource("./output/Customer_new_v4.csv"))
+			.encoding(ENCODING)
+			.delimited().delimiter("\t")
+			.names("Name", "Age", "Gender")
+			.build();
+	}
+
+	@Bean
+	public Step CustomerJdbcCursorStep(JobRepository jobRepository,
+		PlatformTransactionManager transactionManager) throws Exception {
+		log.info("------------------ Init CustomerJdbcCursorStep -----------------");
+
+		return new StepBuilder("CustomerJdbcCursorStep", jobRepository)
+			.<Customer, Customer>chunk(CHUNK_SIZE, transactionManager)
+			.reader(myBatisItemReader())
+			.processor(new CustomerItemProcessor())
+			.writer(CustomerCursorFlatFileItemWriter())
+			.build();
+	}
+
+	@Bean
+	public Job CustomerJdbcCursorPagingJob(Step CustomerJdbcCursorStep, JobRepository jobRepository) {
+		log.info("------------------ Init CustomerJdbcCursorPagingJob -----------------");
+		return new JobBuilder(MYBATIS_CHUNK_JOB, jobRepository)
+			.incrementer(new RunIdIncrementer())
+			.start(CustomerJdbcCursorStep)
+			.build();
+	}
+}
--- src/main/java/com/schooldevops/springbatch/batchstudy/jobs/mybatis/MybatisItemJobConfig.java
+++ src/main/java/com/schooldevops/springbatch/batchstudy/jobs/mybatis/MybatisItemJobConfig.java
@@ -1,13 +1,17 @@
-package com.schooldevops.springbatch.batchstudy.jobs.jpawriter;
+package com.schooldevops.springbatch.batchstudy.jobs.mybatis;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.batch.MyBatisBatchItemWriter;
+import org.mybatis.spring.batch.builder.MyBatisBatchItemWriterBuilder;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.item.database.JpaItemWriter;
-import org.springframework.batch.item.database.builder.JpaItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,22 +22,21 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 import com.schooldevops.springbatch.batchstudy.models.Customer;
 
-import jakarta.persistence.EntityManagerFactory;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-// @Configuration
-public class JpaItemJobConfig {
+@Configuration
+public class MybatisItemJobConfig {
 
 	/**
 	 * CHUNK 크기를 지정한다.
 	 */
 	public static final int CHUNK_SIZE = 100;
 	public static final String ENCODING = "UTF-8";
-	public static final String JPA_ITEM_WRITER_JOB = "JPA_ITEM_WRITER_JOB";
+	public static final String MYBATIS_ITEM_WRITER_JOB = "MYBATIS_ITEM_WRITER_JOB";
 
-	// @Autowired
-	EntityManagerFactory entityManagerFactory;
+	@Autowired
+	SqlSessionFactory sqlSessionFactory;
 
 	@Bean
 	public FlatFileItemReader<Customer> flatFileItemReader() {
@@ -49,10 +52,25 @@ public class JpaItemJobConfig {
 	}
 
 	@Bean
-	public JpaItemWriter<Customer> jpaItemWriter() {
-		return new JpaItemWriterBuilder<Customer>()
-			.entityManagerFactory(entityManagerFactory)
-			.usePersist(true)
+	public MyBatisBatchItemWriter<Customer> mybatisItemWriter() {
+		return new MyBatisBatchItemWriterBuilder<Customer>()
+			.sqlSessionFactory(sqlSessionFactory)
+			.statementId("com.schooldevops.springbatch.batchstudy.jobs.insertCustomers")
+			.build();
+	}
+
+	// @Bean
+	public MyBatisBatchItemWriter<Customer> mybatisItemWriter2() {
+		return new MyBatisBatchItemWriterBuilder<Customer>()
+			.sqlSessionFactory(sqlSessionFactory)
+			.statementId("com.schooldevops.springbatch.batchsample.jobs.insertCustomers")
+			.itemToParameterConverter(item -> {
+				Map<String, Object> parameter = new HashMap<>();
+				parameter.put("name", item.getName());
+				parameter.put("age", item.getAge());
+				parameter.put("gender", item.getGender());
+				return parameter;
+			})
 			.build();
 	}
 
@@ -63,14 +81,14 @@ public class JpaItemJobConfig {
 		return new StepBuilder("flatFileStep", jobRepository)
 			.<Customer, Customer>chunk(CHUNK_SIZE, transactionManager)
 			.reader(flatFileItemReader())
-			.writer(jpaItemWriter())
+			.writer(mybatisItemWriter())
 			.build();
 	}
 
 	@Bean
 	public Job flatFileJob(Step flatFileStep, JobRepository jobRepository) {
 		log.info("------------------ Init flatFileJob -----------------");
-		return new JobBuilder(JPA_ITEM_WRITER_JOB, jobRepository)
+		return new JobBuilder(MYBATIS_ITEM_WRITER_JOB, jobRepository)
 			.incrementer(new RunIdIncrementer())
 			.start(flatFileStep)
 			.build();
